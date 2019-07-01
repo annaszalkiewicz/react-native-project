cbb05a4520b177b2cef48d0f2230af13b5be8b42
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-01 12:10:04 +0200

    Add input to add new place

--- App.js
+++ App.js
@@ -6,29 +6,33 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, TextInput, View} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, TextInput, View } from "react-native";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 export default class App extends Component {
-
   state = {
-    placeName: ''
-  }
+    placeName: ""
+  };
 
-  changeTextHandler = (val) => {
-    this.setState({ placeName: val});
-  }
+  changeTextHandler = val => {
+    this.setState({ placeName: val });
+  };
 
   render() {
     return (
       <View style={styles.container}>
+        <TextInput
+          placeholder="Add an awesome place"
+          value={this.state.placeName}
+          onChangeText={this.changeTextHandler}
+        />
       </View>
     );
   }
@@ -37,18 +41,18 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
