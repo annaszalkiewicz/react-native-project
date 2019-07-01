07c2c18dcbe14ce9e11aea473a00b967bb7894bb
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-02 00:33:31 +0200

    Create submitHandler() that add input value to places state

--- App.js
+++ App.js
@@ -1,31 +1,33 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from "react";
-import { Platform, StyleSheet, TextInput, View, Button } from "react-native";
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+import { StyleSheet, TextInput, View, Button, Text } from "react-native";
 
 export default class App extends Component {
+
   state = {
-    placeName: ""
+    placeName: "",
+    places: []
   };
 
   changeTextHandler = val => {
     this.setState({ placeName: val });
   };
 
+  submitHandler = (e) => {
+    if (this.state.placeName.trim() === '') {
+      return;
+    }
+
+    this.setState((prevState) => {
+      return ({
+        places: [...prevState.places, this.state.placeName]
+      });
+    })
+  }
+
   render() {
+
+    const { places } = this.state;
+
     return (
       <View style={styles.container}>
         <View style={styles.inputContainer}> 
@@ -40,6 +42,7 @@ export default class App extends Component {
             style={styles.buttonAdd}
             title="Add"
             color="#013f25"
+            onPress={this.submitHandler}
           />
         </View>
       </View>
@@ -49,21 +52,10 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
     justifyContent: "center",
     alignItems: 'center',
     backgroundColor: "#F5FCFF",
     padding: 10
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
   },
   inputContainer: {
     width: "100%",
