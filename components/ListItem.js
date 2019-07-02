5699ab8abbfdcc7ad7622d2e8a5863c3649acd40
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-02 12:48:29 +0200

    Add styling to list items

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const ListItem = (props) => {
   return (
     props.places.map((place, i) => (
-      <Text key={i}>{place}</Text>
+      <View 
+        key={i}
+        style={styles.listItem}
+      >
+        <Text
+          style={styles.listItemText}
+        >{place}</Text>
+      </View>
     ))
   )
 }
+
+const styles = StyleSheet.create({
+  listItem: {
+    width: "100%",
+    marginTop: 10,
+    padding: 10,
+    backgroundColor: "#f2f2f2"
+  },
+  listItemText: {
+    color: '#000',
+    fontSize: 16
+  }
+});
 
 export default ListItem;
