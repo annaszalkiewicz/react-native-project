13f7b7775dcfb4c36ad8a200aa0217715dcdd8b0
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-07 03:06:43 +0200

    Clean up code

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
 
 import App from './App';
 import {name as appName} from './app.json';
-import store from './store/store';
+import configureStore from './store/store';
 
-const redux = () => (
+const store = configureStore();
+
+const RNRedux = () => (
   <Provider store={store}>
     <App />
   </Provider>
-)
+);
 
-AppRegistry.registerComponent(appName, () => redux);
+AppRegistry.registerComponent(appName, () => RNRedux);
