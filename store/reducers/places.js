97498b379f0009cddc68d70256e0838389ffc3ee
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-07 03:02:21 +0200

    Clean up code

--- store/reducers/places.js
+++ store/reducers/places.js
@@ -1,52 +1,54 @@
-import PlaceholderImage from '../../assets/placeholder-image.jpg';
+import PlaceholderImage from "../../assets/placeholder-image.jpg";
+import {
+  ADD_PLACE,
+  DELETE_PLACE,
+  SELECT_PLACE,
+  DESELECT_PLACE
+} from "../actions/actionsTypes";
 
 const initialState = {
   places: [],
   selectedPlace: null
-}
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-
-    case 'ADD_PLACE':
+    case ADD_PLACE:
       return {
         ...state,
-        places: [
-          ...state.places,
-          { key: Math.random().toString(), 
-            title: action.placeName,
-            image: PlaceholderImage
-          }
-        ]
-      }
-    
-    case 'DELETE_PLACE':
+        places: state.places.concat({
+          key: Math.random().toString(),
+          name: action.name,
+          image: PlaceholderImage
+        })
+      };
+
+    case DELETE_PLACE:
       return {
         ...state,
         places: state.places.filter(place => {
           return place.key !== state.selectedPlace.key;
         }),
         selectedPlace: null
-      }
-      
-    case 'SELECT_PLACE':
+      };
+
+    case SELECT_PLACE:
       return {
         ...state,
         selectedPlace: state.places.find(place => {
           return place.key === action.key;
         })
-      }
+      };
 
-    case 'DESELECT_PLACE':
+    case DESELECT_PLACE:
       return {
         ...state,
-        selectedPlace: null 
-      }
+        selectedPlace: null
+      };
 
     default:
       return state;
-
   }
-}
+};
 
 export default reducer;
