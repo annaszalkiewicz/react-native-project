2ef490578546652b8958a20cbdca06c451940035
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-07 03:04:06 +0200

    Move changeTextHandler() & submitHandler() to separate component

--- components/NewPlaceForm.js
+++ components/NewPlaceForm.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { Component } from "react";
 import { StyleSheet, View, TextInput, Button } from "react-native";
 
-const NewPlaceForm = (props) => {
-  return (
-    <View style={styles.inputContainer}> 
-          <TextInput 
-            style={styles.input}
-            placeholder="Add an awesome place"
-            value={props.placeName}
-            onChangeText={props.changeTextHandler}
-            underlineColorAndroid="#013f25"
-          />
-          <Button 
-            style={styles.buttonAdd}
-            title="Add"
-            color="#013f25"
-            onPress={props.submitHandler}
-          />
-        </View>
-  )
+class NewPlaceForm extends Component {
+
+  state = {
+    placeName: ""
+  };
+
+  changeTextHandler = val => {
+    this.setState({ placeName: val });
+  };
+
+  submitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+    this.props.onAddedPlace(this.state.placeName);
+  };
+
+  render() {
+
+    return (
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add an awesome place"
+          value={this.state.placeName}
+          onChangeText={this.changeTextHandler}
+          underlineColorAndroid="#013f25"
+        />
+        <Button
+          style={styles.buttonAdd}
+          title="Add"
+          color="#013f25"
+          onPress={this.props.onAddedPlace}
+        />
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
