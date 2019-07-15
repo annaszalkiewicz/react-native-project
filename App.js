f66fc8af095027271c7e2f020d9f725c0d071225
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-15 23:56:26 +0200

    Regsiter my-awesome-places.AuthScreen

--- App.js
+++ App.js
@@ -1,79 +1,91 @@
-import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
-import { connect } from "react-redux";
+import { Navigation } from 'react-native-navigation';
+import AuthScreen from './screens/Auth';
 
-import HeroImage from "./components/HeroImage";
-import List from "./components/List";
-import NewPlaceForm from "./components/NewPlaceForm";
-import PlaceModal from "./components/PlaceModal";
-import {
-  addPlace,
-  deletePlace,
-  selectPlace,
-  deselectPlace
-} from "./store/actions/actionCreators";
+Navigation.registerComponent('my-awesome-places.AuthScreen', () => AuthScreen);
 
-class App extends Component {
-  onAddedPlace = name => {
-    this.props.onAddPlace(name);
-  };
-
-  selectPlace = key => {
-    this.props.onSelectPlace(key);
-  };
-
-  closeModal = () => {
-    this.props.onDeselectPlace();
-  };
-
-  deletePlace = () => {
-    this.props.onDeletePlace();
-  };
-
-  render() {
-    const { places, selectedPlace } = this.props;
-
-    return (
-      <View style={styles.container}>
-        <PlaceModal
-          selectedPlace={selectedPlace}
-          closeModal={this.closeModal}
-          deletePlace={this.deletePlace}
-        />
-        <HeroImage />
-        <NewPlaceForm onAddedPlace={this.onAddedPlace} />
-        <List places={places} selectPlace={this.selectPlace} />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
-    padding: 10
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: 'my-awesome=places.AuthScreen',
+    title: 'Login'
   }
 });
 
-const mapStateToProps = state => {
-  return {
-    places: state.places.places,
-    selectedPlace: state.places.selectedPlace
-  };
-};
+// import React, { Component } from "react";
+// import { StyleSheet, View } from "react-native";
+// import { connect } from "react-redux";
 
-const mapDispatchToProps = dispatch => {
-  return {
-    onAddPlace: name => dispatch(addPlace(name)),
-    onDeletePlace: () => dispatch(deletePlace()),
-    onSelectPlace: key => dispatch(selectPlace(key)),
-    onDeselectPlace: () => dispatch(deselectPlace())
-  };
-};
+// import HeroImage from "./components/HeroImage";
+// import List from "./components/List";
+// import NewPlaceForm from "./components/NewPlaceForm";
+// import PlaceModal from "./components/PlaceModal";
+// import {
+//   addPlace,
+//   deletePlace,
+//   selectPlace,
+//   deselectPlace
+// } from "./store/actions/actionCreators";
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+// class App extends Component {
+//   onAddedPlace = name => {
+//     this.props.onAddPlace(name);
+//   };
+
+//   selectPlace = key => {
+//     this.props.onSelectPlace(key);
+//   };
+
+//   closeModal = () => {
+//     this.props.onDeselectPlace();
+//   };
+
+//   deletePlace = () => {
+//     this.props.onDeletePlace();
+//   };
+
+//   render() {
+//     const { places, selectedPlace } = this.props;
+
+//     return (
+//       <View style={styles.container}>
+//         <PlaceModal
+//           selectedPlace={selectedPlace}
+//           closeModal={this.closeModal}
+//           deletePlace={this.deletePlace}
+//         />
+//         <HeroImage />
+//         <NewPlaceForm onAddedPlace={this.onAddedPlace} />
+//         <List places={places} selectPlace={this.selectPlace} />
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "#F5FCFF",
+//     padding: 10
+//   }
+// });
+
+// const mapStateToProps = state => {
+//   return {
+//     places: state.places.places,
+//     selectedPlace: state.places.selectedPlace
+//   };
+// };
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     onAddPlace: name => dispatch(addPlace(name)),
+//     onDeletePlace: () => dispatch(deletePlace()),
+//     onSelectPlace: key => dispatch(selectPlace(key)),
+//     onDeselectPlace: () => dispatch(deselectPlace())
+//   };
+// };
+
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(App);
