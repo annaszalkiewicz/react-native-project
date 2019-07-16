d6f83a5df83b56db7ee3fe701e90d61a6bd7915b
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-16 23:55:52 +0200

    Add icons to tab based navigation

--- screens/startMainTabs.js
+++ screens/startMainTabs.js
@@ -1,20 +1,30 @@
 import { Navigation } from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const startTabs = () => {
-  Navigation.startTabBasedApp({
-    tabs: [
-      {
-        screen: 'my-awesome-places.SharePlaceScreen',
-        label: 'Share Place',
-        title: 'Share Place'
-      },
-      {
-        screen: 'my-awesome-places.FindPlaceScreen',
-        label: 'Find Place',
-        title: 'Find Place'
-      }
-    ]
-  });
+
+  Promise.all([
+    Icon.getImageSource('share', 30),
+    Icon.getImageSource('search', 30)
+  ]).then(sources => {
+    Navigation.startTabBasedApp({
+      tabs: [
+        {
+          screen: 'my-awesome-places.SharePlaceScreen',
+          label: 'Share Place',
+          title: 'Share Place',
+          icon: sources[0]
+        },
+        {
+          screen: 'my-awesome-places.FindPlaceScreen',
+          label: 'Find Place',
+          title: 'Find Place',
+          icon: sources[1]
+        }
+      ]
+    });
+  })
+  
 }
 
 export default startTabs;
