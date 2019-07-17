60beca318d18bce5009952aa405512d804b22af5
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-18 00:36:37 +0200

    Fix displaying place details when clicking on list item

--- screens/FindPlace.js
+++ screens/FindPlace.js
@@ -6,12 +6,28 @@ import List from '../components/List';
 import HeroImage from '../components/HeroImage';
 
 class FindPlaceScreen extends Component {
+
+  selectPlaceHandler = key => {
+
+    const selected = this.props.places.find(place => {
+      return place.key === key;
+    })
+
+    this.props.navigator.push({
+      screen: 'my-awesome-places.PlaceDetailsScreen',
+      title: selected.name,
+      passProps: {
+        selectedPlace: selected
+      }
+    });
+  }
   render() {
     return (
       <View>
         <HeroImage />
         <List
           places={this.props.places}
+          selectPlace={this.selectPlaceHandler}
         />
       </View>
     )
