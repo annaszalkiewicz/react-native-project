e7cb10123f7bba6c97bbafbf7907e9f6f51d7785
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-17 22:24:41 +0200

    Render NewPlaceForm component in SharePlace screen

--- screens/SharePlace.js
+++ screens/SharePlace.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
+import NewPlaceForm from '../components/NewPlaceForm';
+
 class SharePlaceScreen extends Component {
   render() {
     return (
       <View>
-        <Text>SharePlaceScreen</Text>
+        <NewPlaceForm/>
       </View>
     )
   }
