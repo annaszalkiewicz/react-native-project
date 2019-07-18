6c904744226ad2ae7c0fe57f1435b84b374773d7
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-18 23:42:24 +0200

    Add basic styling to drawer container

--- screens/SideDrawer.js
+++ screens/SideDrawer.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Dimensions, StyleSheet } from 'react-native'
 
 class SideDrawerScreen extends Component {
   render() {
     return (
-      <View>
+      <View style={[styles.drawerContainer, {width: Dimensions.get('window').width * 0.8}]}>
         <Text>SideDrawer</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  drawerContainer: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff'
+  }
+});
 
 export default SideDrawerScreen;
