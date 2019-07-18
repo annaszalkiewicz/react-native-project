54fe7827d276853938190f4b53976a154a4c0bf4
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-18 11:46:53 +0200

    Create SideDrawer screen

---
+++ screens/SideDrawer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class SideDrawerScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>SideDrawer</Text>
+      </View>
+    )
+  }
+}
+
+export default SideDrawerScreen;
