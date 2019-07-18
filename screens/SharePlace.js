cf03dc640ca1d7f6069ddde2b1c4dfe41143fa6e
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-18 23:43:33 +0200

    Add onNavigatorEvent method that toggle drawer on button click

--- screens/SharePlace.js
+++ screens/SharePlace.js
@@ -9,6 +9,25 @@ import HeroImage from '../components/HeroImage';
 
 class SharePlaceScreen extends Component {
 
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent)
+  }
+  
+  onNavigatorEvent = (event) => {
+    console.log(event);
+    
+    if (event.type === 'NavBarButtonPress') {
+      if (event.id === 'sideDrawerToggle') {
+        this.props.navigator.toggleDrawer({
+          side: 'left',
+          animated: true
+        });
+      }
+    }
+    
+  }
+
   onAddedPlace = name => {
     this.props.onAddPlace(name);
   };
