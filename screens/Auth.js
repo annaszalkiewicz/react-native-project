fb311ef8914d1500810f7d4ba3b603abcb99f42e
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-19 22:52:08 +0200

    Add basic styling to auth screen

--- screens/Auth.js
+++ screens/Auth.js
@@ -1,29 +1,42 @@
-import React, { Component } from 'react';
-import { View, Text, Button, TextInput } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, Button, TextInput, StyleSheet } from "react-native";
 
-import startTabs from './startMainTabs';
+import startTabs from "./startMainTabs";
 
 class AuthScreen extends Component {
-
   loginHandler = () => {
     startTabs();
-  }
-  
+  };
+
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Please signup</Text>
-        <Button title="Switch to Login"/>
-        <TextInput placeholder="Your e-mail"/>
-        <TextInput placeholder="Your password" />
-        <TextInput placeholder="Confirm password" />
-        <Button
-          title="Login"
-          onPress={this.loginHandler}
-        />
+        <Button title="Switch to Login" />
+        <View style={styles.inputContainer}>
+          <TextInput placeholder="Your e-mail" style={styles.input} />
+          <TextInput placeholder="Your password" style={styles.input} />
+          <TextInput placeholder="Confirm password" style={styles.input} />
+        </View>
+        <Button title="Login" onPress={this.loginHandler} />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    width: "100%"
+  }
+});
 
 export default AuthScreen;
