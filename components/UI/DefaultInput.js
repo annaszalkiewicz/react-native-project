d9692065b38288c6f260e0ab23be8e2106a7bf47
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-20 22:12:59 +0200

    Remove underlineColorAndroid prop

--- components/UI/DefaultInput.js
+++ components/UI/DefaultInput.js
@@ -3,10 +3,8 @@ import { TextInput, StyleSheet } from 'react-native';
 
 const DefaultInput = props => (
   <TextInput
-    underlineColorAndroid="#fff"
     {...props}
     style={[styles.input, props.style]}
-
   />
 )
 
