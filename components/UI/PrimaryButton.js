af675b72b29dec7ed969ac1c60cdc37a5661dcd2
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-22 20:26:33 +0200

    Use TouchableNativeFeedback for android and TouchableOpacity for other devices

--- components/UI/PrimaryButton.js
+++ components/UI/PrimaryButton.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import React from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+  StyleSheet
+} from "react-native";
 
-const PrimaryButton = (props) => (
-    <TouchableOpacity onPress={props.onPress}>
-      <View style={[styles.button, props.style]}>
-        <Text style={styles.buttonText}>{props.children}</Text>
-      </View>
-    </TouchableOpacity>
-);
+const PrimaryButton = props => {
+  const content = (
+    <View style={[styles.button, props.style]}>
+      <Text style={styles.buttonText}>{props.children}</Text>
+    </View>
+  );
+  if (Platform.OS === "android") {
+    return (
+      <TouchableNativeFeedback onPress={props.onPress}>
+        {content}
+      </TouchableNativeFeedback>
+    );
+  } else {
+    return (
+      <TouchableOpacity onPress={props.onPress}>{content}</TouchableOpacity>
+    );
+  }
+};
 
 const styles = StyleSheet.create({
   button: {
     padding: 10,
     margin: 10,
-    backgroundColor: '#142711',
+    backgroundColor: "#142711",
     borderRadius: 5
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
-    fontWeight: 'bold'
+    fontWeight: "bold"
   }
-})
+});
 
 export default PrimaryButton;
