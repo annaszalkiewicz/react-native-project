c263b32e2fe0507e1f5c20d6fcdd206fc5bfbda7
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-22 19:47:56 +0200

    Add styling to sign out button

--- screens/SideDrawer.js
+++ screens/SideDrawer.js
@@ -18,9 +18,11 @@ class SideDrawerScreen extends Component {
         ]}
       >
         <TouchableOpacity>
-          <View>
-            <Icon name="logout-variant" size={30} color="#000" />
-            <Text>Sign out</Text>
+          <View style={styles.itemContainer}>
+            <View style={styles.icon}>
+              <Icon name="logout-variant" size={30} color="#000" />
+            </View>
+            <Text style={styles.text}>Sign out</Text>
           </View>
         </TouchableOpacity>
       </View>
@@ -33,6 +35,18 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     backgroundColor: "#fff"
+  },
+  itemContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: '#f2f2f2',
+    padding: 10
+  },
+  icon: {
+    width: "15%"
+  },
+  text: {
+    fontSize: 16
   }
 });
 
