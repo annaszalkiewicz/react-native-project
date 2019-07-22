92959583c53a4022210d62c091273fa22f973061
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-22 18:32:17 +0200

    Add sign out button

--- screens/SideDrawer.js
+++ screens/SideDrawer.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react'
-import { Text, View, Dimensions, StyleSheet } from 'react-native'
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 class SideDrawerScreen extends Component {
   render() {
     return (
-      <View style={[styles.drawerContainer, {width: Dimensions.get('window').width * 0.8}]}>
-        <Text>SideDrawer</Text>
+      <View
+        style={[
+          styles.drawerContainer,
+          { width: Dimensions.get("window").width * 0.8 }
+        ]}
+      >
+        <TouchableOpacity>
+          <View>
+            <Icon name="logout-variant" size={30} color="#000" />
+            <Text>Sign out</Text>
+          </View>
+        </TouchableOpacity>
       </View>
-    )
+    );
   }
 }
 
@@ -15,7 +32,7 @@ const styles = StyleSheet.create({
   drawerContainer: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#fff'
+    backgroundColor: "#fff"
   }
 });
 
