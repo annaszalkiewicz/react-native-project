893206caf411b4c9142b09dd6214d6b845f4f35f
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-07-24 01:06:14 +0200

    Add updateMode() that update state viewMode

--- screens/PlaceDetails.js
+++ screens/PlaceDetails.js
@@ -1,42 +1,47 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
-import { connect } from 'react-redux';
+import { Text, View, StyleSheet, Image, TouchableOpacity, Dimensions } from "react-native";
+import { connect } from "react-redux";
 
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import { deletePlace } from '../store/actions/actionCreators';
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { deletePlace } from "../store/actions/actionCreators";
 
 class PlaceDetailsScreen extends Component {
+
+  state = {
+    viewMode: Dimensions.get('window').height > 500 ? 'portrait' : 'landscape'
+  }
+
+  updateMode = () => {
+    this.setState({
+      viewMode: Dimensions.get('window').height > 500 ? 'portrait' : 'landscape'
+    })
+  }
 
   deletePlaceHandler = () => {
     this.props.onDeletePlace(this.props.selectedPlace.key);
     this.props.navigator.pop({
       animated: true,
-      animatioType: 'slide-horizontal'
+      animatioType: "slide-horizontal"
     });
-  }
+  };
   render() {
-
     const { selectedPlace } = this.props;
 
     return (
-      
-          <View style={styles.modalContainer}>
-            <Image source={selectedPlace.image} style={styles.modalImage} />
-            <Text style={styles.modalTitle}>{selectedPlace.name}</Text>
-            <View style={styles.modalButtonsContainer}>
-              <TouchableOpacity onPress={this.deletePlaceHandler}>
-                <View style={styles.deleteButton}>
-                  <Icon
-                    name="delete"
-                    size={20}
-                    color="#fff"
-                  />
-                  <Text style={styles.deleteButtonText}>Delete</Text>
-                </View>
-                
-              </TouchableOpacity>
-            </View>
+      <View style={styles.modalContainer}>
+        <Image source={selectedPlace.image} style={styles.modalImage} />
+        <View>
+          <Text style={styles.modalTitle}>{selectedPlace.name}</Text>
+          <View style={styles.modalButtonsContainer}>
+            <TouchableOpacity onPress={this.deletePlaceHandler}>
+              <View style={styles.deleteButton}>
+                <Icon name="delete" size={20} color="#fff" />
+                <Text style={styles.deleteButtonText}>Delete</Text>
+              </View>
+            </TouchableOpacity>
           </View>
+        </View>
+      </View>
     );
   }
 }
@@ -50,45 +55,48 @@ const styles = StyleSheet.create({
     height: 200
   },
   modalTitle: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 50,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     margin: 20
   },
   modalButtonsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly'
+    flexDirection: "row",
+    justifyContent: "space-evenly"
   },
   deleteButton: {
-    backgroundColor: '#22421D',
+    backgroundColor: "#22421D",
     padding: 16,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center'
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
   },
   deleteButtonText: {
-    color: '#fff',
-    textTransform: 'uppercase',
-    fontWeight: 'bold'
+    color: "#fff",
+    textTransform: "uppercase",
+    fontWeight: "bold"
   },
   closeButton: {
-    backgroundColor: '#428038',
+    backgroundColor: "#428038",
     padding: 16,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center'
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
   },
   closeButtonText: {
-    color: '#fff',
-    textTransform: 'uppercase',
-    fontWeight: 'bold'
+    color: "#fff",
+    textTransform: "uppercase",
+    fontWeight: "bold"
   }
 });
 
 const mapDispatchToProps = dispatch => {
   return {
     onDeletePlace: key => dispatch(deletePlace(key))
-  }
-}
+  };
+};
 
-export default connect(null, mapDispatchToProps)(PlaceDetailsScreen);
+export default connect(
+  null,
+  mapDispatchToProps
+)(PlaceDetailsScreen);
