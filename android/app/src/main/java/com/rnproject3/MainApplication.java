e87e7adc38c7d004aba4b87f89673b93f41c0656
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-08-06 23:25:41 +0200

    Install react native image picker package

--- android/app/src/main/java/com/rnproject3/MainApplication.java
+++ android/app/src/main/java/com/rnproject3/MainApplication.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.airbnb.android.react.maps.MapsPackage;
+import com.imagepicker.ImagePickerPackage;
 
 // public class MainApplication extends Application implements ReactApplication {
 
@@ -62,7 +63,8 @@ public class MainApplication extends NavigationApplication {
 		// No need to add RnnPackage and MainReactPackage
 		return Arrays.<ReactPackage>asList(
 			new VectorIconsPackage(),
-      new MapsPackage()
+			new MapsPackage(),
+			new ImagePickerPackage()
 		);
 	}
 
