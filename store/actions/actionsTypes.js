f7f690b2411aa238d7bbfb4f0bf0c016cefe24ec
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-08-10 15:44:20 +0200

    Add new loading action types

--- store/actions/actionsTypes.js
+++ store/actions/actionsTypes.js
@@ -2,5 +2,7 @@ export const ADD_PLACE = 'ADD_PLACE';
 export const DELETE_PLACE = 'DELETE_PLACE';
 export const UPDATE_MODE = 'UPDATE_MODE';
 export const TRY_AUTH = 'TRY_AUTH';
+export const START_LOADING = 'START_LOADING';
+export const STOP_LOADING = 'STOP_LOADING';
 // export const SELECT_PLACE = 'SELECT_PLACE';
 // export const DESELECT_PLACE = 'DESELECT_PLACE';
