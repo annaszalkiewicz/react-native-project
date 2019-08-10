a9a8045a0480142398b5565bc9e7be02945ea026
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-08-10 15:50:07 +0200

    Combine uiReducer into rootReducer

--- store/reducers/index.js
+++ store/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 
 import places from './places';
+import uiReducer from './uiReducer';
 
-const rootReducer = combineReducers({ places });
+const rootReducer = combineReducers({ places, uiReducer });
 
 export default rootReducer;
