9517989e9fef0b03e514c8ebd9191c97312d4a4e
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-08-15 23:54:47 +0200

    Rewrite tryAuth action, so it doesn't repeat code for signin & signup

--- store/actions/actionCreators.js
+++ store/actions/actionCreators.js
@@ -119,83 +119,43 @@ export const updateMode = () => {
 };
 
 export const tryAuth = (authData, authMode) => {
+  const apiKey = "AIzaSyByiOPOD3HmmaEKEI-xvjqsNbFiT_uNuWg";
+  let url =
+    authMode === "login"
+      ? "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key="
+      : "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
+
   return dispatch => {
-    if (authMode === 'login') {
-      dispatch(signIn(authData));
-    }
-    else {
-      dispatch(signUp(authData));
-    }
-  }
-  // return {
-  //   type: TRY_AUTH,
-  //   authData: authData
-  // };
+    dispatch(startLoading());
+    fetch(url + apiKey, {
+      method: "POST",
+      body: JSON.stringify({
+        email: authData.email,
+        password: authData.password,
+        returnSecureToken: true
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .catch(err => {
+        console.log(err);
+        authMode === "login"
+          ? alert("Sign in failed. Please try again :(")
+          : alert("Sign up failed. Please try again :(");
+        dispatch(stopLoading());
+      })
+      .then(res => res.json())
+      .then(parsedRes => {
+        dispatch(stopLoading());
+        if (parsedRes.error) {
+          alert("Authentication failed. Please try again :(");
+        } else {
+          startTabs();
+        }
+      });
+  };
 };
-
-export const signIn = authData => {
-  return dispatch => {
-    dispatch(startLoading());
-    fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyByiOPOD3HmmaEKEI-xvjqsNbFiT_uNuWg', {
-      method: 'POST',
-      body: JSON.stringify({
-        email: authData.email,
-        password: authData.password,
-        returnSecureToken: true
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      alert('Sign in failed. Please try again :(')
-      dispatch(stopLoading());
-    })
-    .then(res => res.json())
-    .then(parsedRes => {
-      dispatch(stopLoading());
-      if (parsedRes.error) {
-        alert('Sign in failed. Please try again :(');
-      }
-      else {
-        startTabs();
-      }
-    })
-  }
-}
-
-export const signUp = authData => {
-  return dispatch => {
-    dispatch(startLoading());
-    fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyByiOPOD3HmmaEKEI-xvjqsNbFiT_uNuWg', {
-      method: 'POST',
-      body: JSON.stringify({
-        email: authData.email,
-        password: authData.password,
-        returnSecureToken: true
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      alert('Sign up failed. Please try again :(')
-      dispatch(stopLoading());
-    })
-    .then(res => res.json())
-    .then(parsedRes => {
-      dispatch(stopLoading());
-      if (parsedRes.error) {
-        alert('Sign up failed. Please try again :(');
-      }
-      else {
-        startTabs();
-      }
-    })
-  }
-}
 
 // export const selectPlace = key => {
 //   return {
