6448a2cb7eac511c0cc9f5dd4f1d0fb82a24bc0c
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-08-16 13:03:51 +0200

    Combine authReducer into rootReducer

--- store/reducers/index.js
+++ store/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux';
 
 import places from './places';
 import uiReducer from './uiReducer';
+import authReducer from './auth';
 
-const rootReducer = combineReducers({ places, uiReducer });
+const rootReducer = combineReducers({ places, uiReducer, auth: authReducer });
 
 export default rootReducer;
