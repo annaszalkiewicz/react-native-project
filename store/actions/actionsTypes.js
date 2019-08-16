aa9dc54e4b908638a83565c60f3cef0de246d676
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-08-16 12:49:04 +0200

    Add AUTH_SET_TOKEN action type

--- store/actions/actionsTypes.js
+++ store/actions/actionsTypes.js
@@ -4,5 +4,6 @@ export const UPDATE_MODE = 'UPDATE_MODE';
 export const TRY_AUTH = 'TRY_AUTH';
 export const START_LOADING = 'START_LOADING';
 export const STOP_LOADING = 'STOP_LOADING';
+export const AUTH_SET_TOKEN = 'AUTH_SET_TOKEN';
 // export const SELECT_PLACE = 'SELECT_PLACE';
 // export const DESELECT_PLACE = 'DESELECT_PLACE';
