2ca050f9226a29fbe3b08883a2ec072b2f618a06
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-08-19 16:10:40 +0200

    Add START_ADD_PLACE action type

--- store/actions/actionsTypes.js
+++ store/actions/actionsTypes.js
@@ -1,5 +1,6 @@
 export const SET_PLACES = 'SET_PLACES';
 export const PLACE_ADDED = 'PLACE_ADDED';
+export const START_ADD_PLACE = 'START_ADD_PLACE';
 export const REMOVE_PLACE = 'REMOVE_PLACE';
 export const UPDATE_MODE = 'UPDATE_MODE';
 export const TRY_AUTH = 'TRY_AUTH';
